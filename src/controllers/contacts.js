c8d43447ae30c34559643df9cb9c10347f0414af
NataliiaSwan <shapnat69@gmail.com>
2024-07-28 14:29:25 +0300

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -21,10 +21,20 @@ export const getContact = async (req, res) => {
 export const allContacts = async (req, res) => {
   try {
     const contacts = await getAllContacts();
+    const formattedContacts = contacts.map((contact) => ({
+      name: contact.name,
+      phoneNumber: contact.phoneNumber,
+      email: contact.email,
+      isFavourite: contact.isFavourite,
+      contactType: contact.contactType,
+      createdAt: contact.createdAt,
+      updatedAt: contact.updatedAt,
+    }));
+
     res.status(200).json({
       status: 200,
       message: 'Successfully retrieved all contacts!',
-      data: contacts,
+      data: formattedContacts,
     });
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
