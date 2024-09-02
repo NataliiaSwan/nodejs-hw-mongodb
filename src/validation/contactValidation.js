30c62aff319b713e5adbeb63952d9359d82194bb
NataliiaSwan <shapnat69@gmail.com>
2024-09-02 16:59:48 +0300

    Update services

--- src/validation/contactValidation.js
+++ src/validation/contactValidation.js
@@ -4,7 +4,7 @@ export const createContactShema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().optional(),
-  isFavourite: Joi.boolean().optional(),
+  isFavorite: Joi.boolean().optional(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
@@ -12,6 +12,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).optional(),
   phoneNumber: Joi.string().min(3).max(20).optional(),
   email: Joi.string().email().optional(),
-  isFavourite: Joi.boolean().optional(),
+  isFavorite: Joi.boolean().optional(),
   contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 });
