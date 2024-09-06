02e8946fe61202f541c90f5629ef1bc99dbd2fce
NataliiaSwan <shapnat69@gmail.com>
2024-09-06 21:51:34 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,14 +1,15 @@
-import { HttpError } from 'http-errors';
+import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
+  if (isHttpError(err) === true) {
     res.status(err.status).json({
       status: err.status,
       message: err.name,
-      data: err.message,
+      data: err,
     });
     return;
   }
+  console.error(err);
 
   res.status(500).json({
     status: 500,
